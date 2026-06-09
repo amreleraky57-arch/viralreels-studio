375970ec19ebe122e2262b887f983675d854e717
Amr Eleraky <amreleraky57@gmail.com>
2026-06-09 08:19:05 +0300

    Update NM Media website

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { Hero } from "@/components/site/Hero";
 import { Services } from "@/components/site/Services";
 import { VideoShowcase } from "@/components/site/VideoShowcase";
 import { Stats } from "@/components/site/Stats";
-import { Results } from "@/components/site/Results";
 import { About } from "@/components/site/About";
 import { CTA } from "@/components/site/CTA";
 import { Footer } from "@/components/site/Footer";
@@ -20,7 +19,6 @@ export default function App() {
           <Stats />
           <Services />
           <VideoShowcase />
-          <Results />
           <About />
           <CTA />
         </main>
